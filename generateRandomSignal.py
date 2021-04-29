eb4670ecf4d8984e62c36bd4e9cd93377c649049
moamen-gamal <moameneisa@yahoo.com>
2021-04-29 11:05:18 +0200

    generate

--- generateRandomSignal.py
+++ generateRandomSignal.py
@@ -2,21 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 
-frequences=[100,750,1300,1850,2400,2950,3500,4050,4600]
+frequences=[100,750,1300,1850,2200,2750,3300,3850,4400,5500]
 fs=2* max(frequences)
 
 x=np.arange(fs)
-t = np.linspace(0., 1., fs)
 signal=0
-amplitudes=[100,200,500,3360,988,100,7888,6000,1100,8700]
+amplitudes=[1000,5500,2000,3600,9880,1000,8880,6000,1100,7800]
 
-# j=0
+# amplitudes=[2000,5550,1000,3360,988,100,7888,6000,1100,8700]
+
+j=0
 for i in range(len (frequences)):
-    y1=np.sin(2*np.pi*frequences[i]*t)
-    signal=signal+amplitudes[i]*y1
-    # j=j+1
+    y1=np.sin(2*np.pi*frequences[i]*(x/fs))
+    signal=signal+3000*y1
+    j=j+1
 
-
+t = np.linspace(0., 1., fs)
 write("generated.wav", fs, signal.astype(np.int16))
 
 total_ts_sec = len(signal)/fs
